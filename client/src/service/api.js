5f532f83cefca3b78fa6faf3ce07d8db9f70fadb
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-08-21 18:27:52 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -8,7 +8,7 @@ import {
   getType,
 } from "../utils/common-utils";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://authbackend-bgg7.onrender.com/";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
