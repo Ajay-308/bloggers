e1f5723902a159fecac772459b0edf82e0325387
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-08-21 18:29:08 +0530

    Update Comment.jsx

--- client/src/components/details/comments/Comment.jsx
+++ client/src/components/details/comments/Comment.jsx
@@ -49,7 +49,7 @@ const Comment = ({ comment, setToggle }) => {
         let config = {
             method: 'delete',
             maxBodyLength: Infinity,
-            url: `http://localhost:5000/comment/delete/${comment._id}`,
+            url: `https://authbackend-bgg7.onrender.com/comment/delete/${comment._id}`,
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': sessionStorage.getItem('accessToken'),
